0996e40c0738f5edcbb00271d381e65684f1d3f2
hohyenho <75921113+hohyenho@users.noreply.github.com>
2026-07-09 11:04:25 +0900

    Add files via upload

---
+++ src/pages/ClassPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ClassPage = () => {
+  return (
+    <div>
+      ClassPage 영역입니다.
+    </div>
+  )
+}
+
+export default ClassPage
---
+++ src/pages/Todo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Todo = () => {
+  return (
+    <div>
+      Todo 영역입니다.
+    </div>
+  )
+}
+
+export default Todo
